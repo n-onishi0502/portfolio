c09a8f9c25c40ace0d526076aa14cca3513c4f6c
DESKTOP-C8K947F\WIN065 <n.onishi@l-owcal.com>
2024-02-09 12:25:21 +0900

    no message

--- introduce/vue.config.js
+++ introduce/vue.config.js
@@ -1,11 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
- ?  '/app/'
- : './'
 })
 // 追加
-// module.exports = {
-//   publicPath: './'
-// }
+module.exports = {
+  publicPath: './'
+}
+
+// module.exports = defineConfig({
+//   publicPath: process.env.NODE_ENV === 'production'
+//  ?  '/app/'
+//  : './'
+//  })
